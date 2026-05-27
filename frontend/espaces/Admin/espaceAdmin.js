a1ca980923266fda79d78e0128b658500fb07f2b
greg-hue <gregoire.nogier@edu.ece.fr>
2026-05-27 10:48:07 +0200

    Nouveau php dashboard prof

--- frontend/espaces/Admin/espaceAdmin.js
+++ frontend/espaces/Admin/espaceAdmin.js
@@ -1,12 +1,9 @@
-document.addEventListener("DOMContentLoaded", () => {
+document.addEventListener("DOMContentLoaded", async () => {
 
-    const utilisateur = JSON.parse(
-        localStorage.getItem("utilisateurConnecte")
-    );
+    const utilisateur = JSON.parse(localStorage.getItem("utilisateurConnecte"));
 
-    // Sécurité
     if (!utilisateur || utilisateur.role !== "admin") {
-        window.location.href = "../authentification.html";
+        window.location.href = "../../authentification.html";
         return;
     }
 
@@ -16,13 +13,37 @@ document.addEventListener("DOMContentLoaded", () => {
     document.getElementById("roleUtilisateur").textContent =
         "Administrateur";
 
-    // Déconnexion
-    document.getElementById("btnDeconnexion")
-    .addEventListener("click", () => {
-
+    document.getElementById("btnDeconnexion").addEventListener("click", () => {
         localStorage.removeItem("utilisateurConnecte");
-
         window.location.href = "../../authentification.html";
     });
 
+    const reponse = await fetch(
+        "../../../backend/Admin/getAdminDashboard.php"
+    );
+
+    const resultat = await reponse.json();
+
+    if (resultat.success) {
+        document.getElementById("totalEtudiants").textContent =
+            resultat.stats.total_etudiants;
+
+        document.getElementById("totalProfs").textContent =
+            resultat.stats.total_profs;
+
+        document.getElementById("totalGroupes").textContent =
+            resultat.stats.total_groupes;
+
+        const liste = document.getElementById("listeDerniersUtilisateurs");
+
+        resultat.utilisateurs.forEach(utilisateur => {
+            liste.innerHTML += `
+                <tr>
+                    <td>${utilisateur.prenom} ${utilisateur.nom}</td>
+                    <td>${utilisateur.role}</td>
+                    <td>${utilisateur.date_creation}</td>
+                </tr>
+            `;
+        });
+    }
 });
