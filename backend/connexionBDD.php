04c4a5c531a8ea59f83d25598b84b1c9b8b43606
eloi-kgg <eloi.gaillard@edu.ece.fr>
2026-05-31 12:16:38 +0200

    Update connexionBDD.php

--- backend/connexionBDD.php
+++ backend/connexionBDD.php
@@ -1,5 +1,5 @@
 <?php
-
+//Variables de configuration pour la BDD
 $host = "localhost";
 $dbname = "ifsi_smartcampus";
 $username = "root";
@@ -8,22 +8,27 @@ $port = "3306";
 
 try {
 
+//Tentative de connexion avec l'objet PDO et encodage utf8
     $bdd = new PDO(
+        
         "mysql:host=$host;port=$port;dbname=$dbname;charset=utf8",
         $username,
         $password
     );
 
+    //Active la gestion des erreurs SQL sous forme d'exceptions
     $bdd->setAttribute(PDO::ATTR_ERRMODE, PDO::ERRMODE_EXCEPTION);
 
 } catch (PDOException $e) {
 
+//Gere le cas ou la connexion a la BDD foire et renvoie l'erreur en json
     echo json_encode([
         "success" => false,
         "message" => "Erreur connexion BDD",
         "details" => $e->getMessage()
     ]);
 
+    //Arrete l'execution du script
     exit;
 }
 ?>
