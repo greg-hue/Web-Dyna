ccf84610c7b4cbcb1a5b97bd5469d5d6cd4c21cc
eloi-kgg <eloi.gaillard@edu.ece.fr>
2026-05-31 12:24:58 +0200

    Update getNews.php

--- backend/getNews.php
+++ backend/getNews.php
@@ -1,8 +1,12 @@
 <?php
+
+//Configure la reponse pour renvoyer du json au frontend
 header("Content-Type: application/json");
 
+//Inclusion du fichier pour se connecter a la BDD
 require_once "connexionBDD.php";
 
+//Execution de la requete pour recup toutes les news triées par la plus recente
 $requete = $bdd->query("
     SELECT
         titre,
@@ -14,8 +18,10 @@ $requete = $bdd->query("
     ORDER BY date_publication DESC
 ");
 
+//Recup de toutes les news sous forme de tableau
 $news = $requete->fetchAll(PDO::FETCH_ASSOC);
 
+//Envoi des news en json au frontend
 echo json_encode([
     "success" => true,
     "news" => $news
